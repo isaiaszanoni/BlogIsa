d579d39b7600905674ce5afccfcb4a0805ecb918
isaiaszanoni <isaiasz.mus@gmail.com>
2021-09-04 11:27:57 -0300

    Adicionando endpoints ao PostagemControlador

--- isaBlog/src/main/java/com/isaBlog/isaBlog/controladores/PostagemControlador.java
+++ isaBlog/src/main/java/com/isaBlog/isaBlog/controladores/PostagemControlador.java
@@ -2,11 +2,18 @@ package com.isaBlog.isaBlog.controladores;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,13 +40,24 @@ public class PostagemControlador {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("/teste")
-	public String testando() {
-		return "Isso é um teste - Postagem";
-	}
-	
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity <Object> GetByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repositorio.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> novaPostagem(@RequestBody Postagem addPost) {
+		return ResponseEntity.status(201).body(repositorio.save(addPost));
+	}
+	
+	@DeleteMapping("/deleta/{idPostagem}")
+	public void deletePostagem (@PathVariable(value = "idPostagem") Long iddaPostagem) {
+		repositorio.deleteById(iddaPostagem);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> atualizaPostagem(@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(repositorio.save(postagem));
+	}
+
 }
