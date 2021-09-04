c2386ee9810cd78f251ba6a1f25ab4775fcd11e4
isaiaszanoni <isaiasz.mus@gmail.com>
2021-09-04 13:28:04 -0300

    Adicionando os Endpoints ao TemaControlador

--- isaBlog/src/main/java/com/isaBlog/isaBlog/controladores/TemaControlador.java
+++ isaBlog/src/main/java/com/isaBlog/isaBlog/controladores/TemaControlador.java
@@ -2,10 +2,16 @@ package com.isaBlog.isaBlog.controladores;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,9 +36,18 @@ public class TemaControlador {
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@GetMapping("/teste")
-	public String testando() {
-		return "Isso é só um teste - Tema";
+	@PostMapping
+	public ResponseEntity<Tema> addTema(@Valid @RequestBody Tema novoTema) {
+		return ResponseEntity.status(201).body(repositorio.save(novoTema));
 	}
-
+	
+	@DeleteMapping("/deleta/{idTema}")
+	public void deletaTema(@PathVariable (value = "idTema") Long idTema) {
+		repositorio.deleteById(idTema);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Tema> atualizaTema(@Valid @RequestBody Tema atualizaTema) {
+		return ResponseEntity.ok().body(repositorio.save(atualizaTema));
+	}
 }
