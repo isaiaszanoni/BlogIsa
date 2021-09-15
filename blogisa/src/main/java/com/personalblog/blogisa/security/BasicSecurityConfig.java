269bc8493ccdd0e2bc774698ad96f16b6ca9da51
isaiaszanoni <isaiasz.mus@gmail.com>
2021-09-15 20:43:40 -0300

    Adicionando a classe UserDetailsService de Security

--- blogisa/src/main/java/com/personalblog/blogisa/security/BasicSecurityConfig.java
+++ blogisa/src/main/java/com/personalblog/blogisa/security/BasicSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,7 +28,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
     }
 	
 	@Override
-	protected void condigure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
         .antMatchers(HttpMethod.POST, "/logar").permitAll()
         .antMatchers(HttpMethod.PUT, "/cadastrar").permitAll()
---
+++ blogisa/src/main/java/com/personalblog/blogisa/security/UserDetailsImplements.java
@@ -0,0 +1,80 @@
+package com.personalblog.blogisa.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.personalblog.blogisa.model.Usuario;
+
+public class UserDetailsImplements implements UserDetails{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String userName;
+	private String password;
+	
+	public UserDetailsImplements(Usuario user) {
+		this.userName = user.getNomeUsuario();
+		this.password = user.getSenhaUsuario();
+	}
+	
+	public UserDetailsImplements() {
+		
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
